54224145c0086802e4fdff108fee98add0980fe1
Evandrolds <evandro.lds.ds@outlook.com>
2023-07-13 19:36:29 -0300

    Quadrante de cordenadas cartesina

---
+++ src/algoritmos/Quadrante.java
@@ -0,0 +1,30 @@
+package algoritmos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Evandro
+ */
+public class Quadrante {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            
+            if (x > 0 && y > 0) {
+                System.out.println("primeiro");
+            } else if (x < 0 && y > 0) {
+                System.out.println("segundo");
+            } else if (x < 0 && y < 0) {
+                System.out.println("terceiro");
+            } else if (x > 0 && y < 0) {
+                System.out.println("quarto");
+            } else {
+                break;
+            }
+        }
+    }
+}
